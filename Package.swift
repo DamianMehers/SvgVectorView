2b3dc52efab833939dc6ca8d8fdb178243947379
damian <damian@mehers.com>
2021-04-04 15:12:54 +0200

    Make package available on all platforms

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SvgVectorView",
     platforms: [
-            .macOS(.v10_15)
+            .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)
         ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
